02e9dbd2754df9b15c42809440c7f5a80e964d2c
Bumbastlk <92792518+Tetrest@users.noreply.github.com>
2024-01-12 17:27:13 +0200

    init2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:pmp/main.dart';
+import 'package:kudelia_pmp_laba_1/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
